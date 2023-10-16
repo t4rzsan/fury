48e75078f80c31d03dea2f52668461f0dcf59823
Jakob Christensen <jakob.christensen@hey.com>
2023-10-16 21:36:37 +0200

    Init

---
+++ src/Fury/FixedSizeArrayPool.fs
@@ -0,0 +1,28 @@
+﻿namespace Fury.Buffer
+
+open System
+
+module FixedSizeArrayPool =
+    type  FixedSizeArrayPool<'T>(arraySize: int, capacity: int) =
+        let mutable pool : 'T[] [] = Array.zeroCreate capacity
+        let mutable currentIndex = capacity - 1
+
+        do
+            for i in 0 .. capacity - 1 do
+                pool.[i] <- Array.zeroCreate arraySize
+    
+        member this.Rent() =
+            if currentIndex = 0 then
+                raise (IndexOutOfRangeException("Capacity has been reached."))
+
+            let index =  currentIndex
+            currentIndex <- currentIndex - 1
+            pool.[index]
+
+        member this.TryRent() =
+            if currentIndex = 0 then
+                None
+            else
+                let index =  currentIndex
+                currentIndex <- currentIndex - 1
+                Some pool.[index]
